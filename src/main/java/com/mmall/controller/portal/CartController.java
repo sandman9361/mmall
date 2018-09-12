530bd3800ab43420fc97339fa12d972148893f94
imooc <379455169@qq.com>
2018-09-12 17:51:06 +0800

    mmall商城

--- src/main/java/com/mmall/controller/portal/CartController.java
+++
@@ -1,7 +0,0 @@
-package com.mmall.controller.portal;
-
-public class CartController {
-    private String goodName;
-    private double price;
-    private String type;
-}
--- src/main/java/com/mmall/controller/portal/SaleR.java
+++
@@ -1,7 +0,0 @@
-package com.mmall.controller.portal;
-
-public class SaleR {
-    private String goodName;
-    private double price;
-    private String type;
-}
